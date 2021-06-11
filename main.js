df913247b15d3bd962938cca59887559226a183e
Shubhan28 <83396802+Shubhan28@users.noreply.github.com>
2021-06-11 21:11:11 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,22 @@
 width = 400;
 height = 400;
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
 function preload()
 {
-   canvas = createCanvas(width, height);
+   song1 = loadSound("music.mp3");
+   song2 = loadSound("music2.mp3");
+}
+function setup()
+{
+    canvas = createCanvas(width, height);
    canvas.center();
 
    video = createCapture(VIDEO);
    video.hide();
+
+   classifier = ml5.poseNet(video, modelLoaded);
+   classifier.on('pose', gotposes);
 }
